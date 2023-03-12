d752016fb9a4da97d9a15d7c02d8ce407e72f14e
rasko1 <oophn.88@yandex.ru>
2023-03-12 12:39:41 +0300

    was rename variable : StartScore -> startScore

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        int StartScore = 100;
+        int startScore = 100;
         int deposit = 1500;
         int bonus;
         if (deposit > 1000) {
-            bonus = deposit / StartScore;
+            bonus = deposit / startScore;
         } else {
             bonus = 0;
         }
 
-        int balance = StartScore + deposit + bonus;
+        int balance = startScore + deposit + bonus;
         System.out.println("Спасибо за ваш платеж на сумму " + deposit + "р.\n Баланс: " + balance + "р.\n Ваш бонус составляет: " + bonus + "р.");
     }
 }
